2c56d5ed928c422cee72f4b0ee7b8ffa7f65634f
SultonbekPGMR <sultonbektolanov60@gmail.com>
2024-04-26 11:52:08 +0500

    test

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,7 +2,9 @@ plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
 }
-// salom 
+// salom
+
+
 android {
     namespace = "uz.sultonbek1547.hackathonproject2024_innovatex"
     compileSdk = 34
@@ -40,6 +42,7 @@ dependencies {
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.11.0")
+    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
---
+++ app/src/main/java/uz/sultonbek1547/hackathonproject2024_innovatex/ui/MainActivity.kt
@@ -0,0 +1,13 @@
+package uz.sultonbek1547.hackathonproject2024_innovatex.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import uz.sultonbek1547.hackathonproject2024_innovatex.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+}
