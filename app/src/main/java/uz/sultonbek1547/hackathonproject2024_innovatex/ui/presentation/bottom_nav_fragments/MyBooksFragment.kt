6efa38739a3a0998e06cefb6f86e85e625f1d546
SultonbekPGMR <sultonbektolanov60@gmail.com>
2024-04-26 12:37:06 +0500

    firebase added

--- app/src/main/java/uz/sultonbek1547/hackathonproject2024_innovatex/ui/presentation/bottom_nav_fragments/MyBooksFragment.kt
+++ app/src/main/java/uz/sultonbek1547/hackathonproject2024_innovatex/ui/presentation/bottom_nav_fragments/MyBooksFragment.kt
@@ -5,13 +5,19 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import uz.sultonbek1547.hackathonproject2024_innovatex.databinding.FragmentExploreBinding
+import uz.sultonbek1547.hackathonproject2024_innovatex.databinding.FragmentChatsBinding
 import uz.sultonbek1547.hackathonproject2024_innovatex.databinding.FragmentMyBooksBinding
 
+// TODO: Rename parameter arguments, choose names that match
+// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+private const val ARG_PARAM1 = "param1"
+private const val ARG_PARAM2 = "param2"
 
-class ExploreFragment : Fragment() {
 
-    private val binding by lazy { FragmentExploreBinding.inflate(layoutInflater) }
+class MyBooksFragment : Fragment() {
+
+
+    private val binding by lazy { FragmentMyBooksBinding.inflate(layoutInflater) }
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?,
