1f083d073336608526b484d6086892b4d2d01aea
Abdujabbor <ghub9472@gmail.com>
2024-04-26 12:11:22 +0500

    init

--- app/src/main/java/uz/sultonbek1547/hackathonproject2024_innovatex/ui/main/MainActivity.kt
+++ app/src/main/java/uz/sultonbek1547/hackathonproject2024_innovatex/ui/main/MainActivity.kt
@@ -3,12 +3,14 @@ package uz.sultonbek1547.hackathonproject2024_innovatex.ui.main
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import uz.sultonbek1547.hackathonproject2024_innovatex.R
+import uz.sultonbek1547.hackathonproject2024_innovatex.databinding.ActivityMainBinding
 
-class dMainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity() {
+    val binding by lazy{ ActivityMainBinding.inflate(layoutInflater)}
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
         // salom
     }
 
